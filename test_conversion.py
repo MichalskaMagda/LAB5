c60a2d8610a9cd63894d386894e06bd868aa73c1
Magdalena Michalska <magda.michalska@hotmail.com>
2025-05-20 23:26:12 +0200

    Change files

--- test_conversion.py
+++ test_conversion.py
@@ -1,19 +1,19 @@
 import pytest
-from utils import convert_to_binary
+from utils import conversion
 
 
 @pytest.mark.parametrize("number, expected", [(0, "0"), (1, "1"), (100, "1100100")])
 def test_valid_numbers(number, expected):
-    assert convert_to_binary(number) == expected
+    assert conversion(number) == expected
 
 
 def test_out_of_range():
     with pytest.raises(ValueError):
-        convert_to_binary(-1)
+        conversion(-1)
     with pytest.raises(ValueError):
-        convert_to_binary(101)
+        conversion(101)
 
 
 def test_non_integer():
     with pytest.raises(TypeError):
-        convert_to_binary(10.5)
+        conversion(10.5)
